8fc129118444a083eb50e57ff2ad5db0cb530d09
Scott Jason <scottleejason@gmail.com>
2015-12-25 18:04:17 -0800

    refactors session store, swapping flux with reflux

--- client/scripts/components/Session.js
+++ client/scripts/components/Session.js
@@ -13,55 +13,55 @@ var Session = React.createClass({
   getInitialState: function() {
     return { session: actions.getSession() }
   },  
+  componentDidMount: function() {
+    this.listenTo(ConnectStore, this.onSessionReceived);
+  },
   onSessionReceived: function(session) {
     console.log("session received in session", session);
     this.setState({ session: session });
-    console.log("session state", this.state);
-  },  
-  componentDidMount: function() {
-    this.listenTo(ConnectStore, this.onSessionReceived);  
-    // var session = OT.initSession(this.state.session.key, this.state.session.sessionId);
-    // this.registerEvents(session);
-    // session.connect(this.state.session.token, function(error) {
-    //   if (error) {
-    //     console.log(error.message);
-    //   } else {
-    //     var opts = {
-    //       Animator: {
-    //         duration: 500,
-    //         easing: 'swing'
-    //       },
-    //       bigFixedRatio: false
-    //     };
-    //     var layoutContainer = document.getElementById('layoutContainer');
-    //     var layout = initLayoutContainer(layoutContainer, opts).layout;
-    //     session.publish("publisherContainer");
-    //     layout();
-    //     var resizeTimeout;
-    //     window.onresize = function() {
-    //       clearTimeout(resizeTimeout);
-    //       resizeTimeout = setTimeout(function() {
-    //         layout();
-    //       }, 20);
-    //     };
-    //   }
-    // });
+    this.startBroadcast();
   },
+  startBroadcast: function() {
+    var session = OT.initSession(this.state.session.key, this.state.session.sessionId);
+    this.registerEvents(session);
+    session.connect(this.state.session.token, function(error) {
+      if (error) {
+        console.log(error.message);
+      } else {
+        var opts = {
+          Animator: {
+            duration: 500,
+            easing: 'swing'
+          },
+          bigFixedRatio: false
+        };
+        var layoutContainer = document.getElementById('layoutContainer');
+        var layout = initLayoutContainer(layoutContainer, opts).layout;
+        session.publish("publisherContainer");
+        layout();
+        var resizeTimeout;
+        window.onresize = function() {
+          clearTimeout(resizeTimeout);
+          resizeTimeout = setTimeout(function() {
+            layout();
+          }, 20);
+        };
+      }
+    });
+  },  
   registerEvents: function(session) {
     var connectionCount = 0;
     session.on({
       connectionCreated: function(event) {
         connectionCount++;
-        // SessionActions.addViewer('addViewer', connectionCount);
-        console.log(connectionCount + ' connections.');
+        console.log('connection created: ', connectionCount + ' connections.');
       },
       connectionDestroyed: function(event) {
         connectionCount--;
-        // SessionActions.removeViewer('addViewer', connectionCount);
-        console.log(connectionCount + ' connections.');
+        console.log('connection created: ', connectionCount + ' connections.');
       },
       sessionDisconnected: function sessionDisconnectHandler(event) {
-        console.log('Disconnected from the session.', event.reason);
+        console.log('Disconnected from the session: ', event.reason);
       }
     });
   },
